cbbcd7c4955a114770a986d0b6f5696ae9e975cc
CobraOmega <yashad04@gmail.com>
2025-01-23 14:39:32 +0530

    DevTrack: Files: Basic_Calculator.py. [main]  | Changes in: Basic_Calculator.py (modified print, if; added url, verify_ssl, name, django, python_version, entity, def, self, monster, super, return, print, num1, num2, operator; removed n, else, a, b, c, elif)

--- Python/AC_LEC2.py
+++ Python/AC_LEC2.py
@@ -1,24 +1,46 @@
-#WAP to check if a number entered by user is odd or even
-n = int(input("Enter a number: "))
-if(n % 2):
-    print("The number is even.")
-else:
-    print("The number is odd.")
+##WAP to check if a number entered by user is odd or even
+#n = int(input("Enter a number: "))
+#if(n % 2):
+#    print("The number is even.")
+#else:
+#    print("The number is odd.")
+#
+##WAP to find the greatest of 3 numbers entered by the user.
+#a = float(input("Enter a number: "))
+#b = float(input("Enter a number: "))
+#c = float(input("Enter a number: "))
+#if(a > b and a > c):
+#    print("The greatest number out of the provided numbers is: ", a)
+#elif(b > a and b > c):
+#    print("The greatest number out of the provided numbers is: ", b)
+#elif(c > a and c > b):
+#    print("The greatest number out of the provided numbers is: ", c)
+#
+##WAP to check if a number is multiple of  or not
+#a = int(input("Enter a number: "))
+#if(a % 7 == 0):
+#    print("The number is multiple of 7.")
+#else:
+#    print("The number is not a multiple of 7.")
 
-#WAP to find the greatest of 3 numbers entered by the user.
-a = float(input("Enter a number: "))
-b = float(input("Enter a number: "))
-c = float(input("Enter a number: "))
-if(a > b and a > c):
-    print("The greatest number out of the provided numbers is: ", a)
-elif(b > a and b > c):
-    print("The greatest number out of the provided numbers is: ", b)
-elif(c > a and c > b):
-    print("The greatest number out of the provided numbers is: ", c)
+class entity:
+    def __init__(self, damage):
+        self.damage = damage
 
-#WAP to check if a number is multiple of  or not
-a = int(input("Enter a number: "))
-if(a % 7 == 0):
-    print("The number is multiple of 7.")
-else:
-    print("The number is not a multiple of 7.")
+    def attack(self):
+        print(f"attack {self.damage}")
+
+class monster(entity):
+    def __init__(self, damage, health):
+        super().__init__(damage)
+        self.health = health
+        self.damage = damage
+
+    def __str__(self):
+        return f"A monster with {self.health} hp"
+
+monster = monster(30, 100)
+monster.attack()
+print(monster)
+        
+    
---
+++ Python/Basic_Calculator.py
@@ -0,0 +1,9 @@
+print("*************** Basic Calculator ***************")
+
+num1 = input("Enter First number: ")
+num2 = input("Enter Second number: ")
+operator = input("Enter Operator(+, -, /, *): ")
+
+if operator == '+':
+    print(num1 + num2)
+
