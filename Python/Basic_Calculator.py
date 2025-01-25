1e6576b99ef399dddf68666146142ab1a230ed47
CobraOmega <yashad04@gmail.com>
2025-01-25 19:58:33 +0530

    DevTrack: Initial commit with existing project files

--- Python/Basic_Calculator.py
+++ Python/Basic_Calculator.py
@@ -1,9 +1,17 @@
 print("*************** Basic Calculator ***************")
 
-num1 = input("Enter First number: ")
-num2 = input("Enter Second number: ")
+num1 = float(input("Enter First number: "))
+num2 = float(input("Enter Second number: "))
 operator = input("Enter Operator(+, -, /, *): ")
 
 if operator == '+':
     print(num1 + num2)
 
+elif operator == '-':
+    print(num1 - num2)
+
+elif operator == '/':
+    print(num1 / num2)
+
+elif operator == '*':
+    print(num1 * num2)
